d51b27ec7f01055bd45784d3aac8011bbcc4a7e5
follijulio <follijulio@gmail.com>
2025-02-12 22:03:04 -0300

    add ui's components

--- src/ui/cards/Card.tsx
+++ src/ui/cards/Card.tsx
@@ -1,6 +1,9 @@
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
+
 const Card: React.FC = () => {
   return (
     <div className="h-40 w-60 bg-slate-600 flex rounded-2xl border border-white text-xl font-light p-2">
+        <LoadingSpinner  />
         card
     </div>
   );
