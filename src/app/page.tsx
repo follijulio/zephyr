99b39aa8a6d6d0318b945708fe0a08ea1e100811
follijulio <follijulio@gmail.com>
2025-02-12 21:20:27 -0300

    add Card component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import Card from "@/ui/cards/Card";
+
+
 export default function Home() {
   return (
     <div className="flex flex-col h-screen w-screen justify-center items-center bg-blue-950 text-white text-6xl font-extrabold">
+      <Card />
       Zephyr!
     </div>
   );
---
+++ src/ui/cards/Card.tsx
@@ -0,0 +1,10 @@
+const Card: React.FC = () => {
+  return (
+    <div className="h-40 w-60 bg-slate-600 flex rounded-2xl border border-white text-xl font-light p-2">
+        card
+    </div>
+  );
+};
+
+
+export default Card;
