235ac8ffbbf540eb3c002a93f3e1a50c0c68177b
follijulio <follijulio@gmail.com>
2025-02-21 22:47:50 -0300

    refatorar componente Home para usar função de componente com tipagem React.FC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-export default function Home() {
-  return ( 
+const page: React.FC = () => {
+  return (
     <div className="flex flex-col justify-center items-center text-white text-6xl font-extrabold h-full w-full">
       Zephyr!
     </div>
   );
-}
+};
+export default page;
