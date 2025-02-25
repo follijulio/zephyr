bde32a1c77ed91a14110a8f08f3ecb4f5254e8b8
follijulio <follijulio@gmail.com>
2025-02-25 12:44:48 -0300

    refatorar classe Note para incluir userId e métodos de serialização/deserialização

--- src/lib/types/note.ts
+++ src/lib/types/note.ts
@@ -1,13 +1,35 @@
 export class Note {
-  id: string;
+  id?: string;
   note: string;
   title: string;
   actived: boolean;
+  userId: string;
 
-  constructor(note: string, title: string, actived: boolean, id: string) {
-    this.id = id
+  constructor(
+    note: string,
+    title: string,
+    actived: boolean,
+    userId: string,
+    id?: string
+  ) {
+    this.id = id;
     this.note = note;
     this.title = title;
     this.actived = actived;
+    this.userId = userId;
+  }
+
+  serialize() {
+    return {
+      id: this.id,
+      note: this.note,
+      title: this.title,
+      actived: this.actived,
+      userId: this.userId
+    };
+  }
+
+  deserialize(data: Note): Note {
+    return new Note(data.note, data.title, data.actived, data.userId, data.id);
   }
 }
